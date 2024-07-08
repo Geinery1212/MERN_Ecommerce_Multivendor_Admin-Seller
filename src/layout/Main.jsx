70357bbc30ea5206870d701deac32c1d354341f4
ervin-diaz <Geineryodan@gmail.com>
2024-07-08 15:11:37 -0600

    Finished header  design

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import { Sidebar } from './Sidebar'
 
 const Main = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
     return (
         <div className='bg-[#cdcae9] w-full min-h-screen'>
-            <Header />
-            <Sidebar />
-            <div className='ml-0 lg:ml-[260px] pt-95px transition-all'>
+            <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+            <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+            <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
                 <Outlet></Outlet>
             </div>
         </div>
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom'
 import logo from '../images/logo.png'
 import { getNav } from '../navigation';
 import { IoLogOut } from "react-icons/io5";
-export const Sidebar = () => {
+export const Sidebar = ({ showSidebar, setShowSidebar }) => {
     const [allNav, setAllNav] = useState([]);
     const { pathname } = useLocation();
     useEffect(() => {
@@ -13,10 +13,10 @@ export const Sidebar = () => {
     }, []);
     return (
         <div>
-            <div>
+            <div onClick={() => setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}>
 
             </div>
-            <div className='w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all'>
+            <div className={`${showSidebar ? 'left-0' : '-left-[260px] lg:left-0'} w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}>
                 <div className='h-[70px] flex justify-center items-center'>
                     <Link to='/' className='w-[180px] h-[50px]'>
                         <img src={logo} alt="Logo" className='w-full h-full' /></Link>
